83355e2fb90724a9ee04411a1e906adcc8e11ef4
DiasHafizhan <diashafizhan32@gmail.com>
2025-10-15 20:07:03 +0700

    second push

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { FaArrowDown } from "react-icons/fa"
+import { IoMdDownload } from "react-icons/io"
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="grid grid-cols-1 md:grid-cols-2 items-center pt-10 xl:gap-0 gap-6">
+        <div className="">
+          <div className="bg-zinc-800 w-fit flex items-center gap-3 rounded-2xl p-4 mb-6">
+            <img
+              src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=580"
+              alt=""
+              className="w-10 rounded-md"
+            />
+            <q>Kode yang indah, lahir dari ketekunan 😁</q>
+          </div>
+          <h1 className="text-5xl/tight font-bold mb-6">Hi, Saya Dias Hafizhan</h1>
+          <p className="text-base/loose mb-6 opacity-50">
+            Saya mempunyai ketertarikan dalam bidang programming dan designer, terutama pada pembuatan website dan design poster, pamflet serta banner, ketertarikan pada bidang ini sudah berlangsung labih dari 4 tahun untuk semua bidang
+          </p>
+          <div className="flex items-center sm:gap-4 gap-2">
+            <a href="" className="bg-violet-700 p-4 rounded-2xl hover:bg-violet-600 flex gap-2 items-center">
+              Download CV <IoMdDownload className="ri-lg" />
+            </a>
+            <a href="" className="bg-zinc-700 p-4 rounded-2xl hover:bg-zinc-600 flex gap-2 items-center">
+              Lihat Proyek <FaArrowDown />
+            </a>
+          </div>
+        </div>
+
+        <img
+          src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=580"
+          alt=""
+          className="w-[500px] md:ml-auto"
+        />
+
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,52 @@
+import React, { useEffect, useState } from 'react'
+
+const Navbar = () => {
+
+  const [active, setActive] = useState(false)
+
+  useEffect(() => {
+    const handleScroll = () => {
+      // Logika: Jika posisi scroll Y (vertikal) melebihi 150 piksel...
+      if (window.scrollY > 150) {
+        // ...atur state 'active' menjadi true
+        setActive(true)
+      } else {
+        // ...jika tidak, atur 'active' menjadi false
+        setActive(false)
+      }
+    };
+
+
+    window.addEventListener("scroll", handleScroll)
+    return () => {
+      window.removeEventListener("scroll", handleScroll)
+    }
+
+  }, [])
+
+  return (
+    <div className='py-7 flex items-center justify-between'>
+      <div className="">
+        <h1 className='text-3xl font-bold bg-white text-black p-1 md:bg-transparent md:text-white'>
+          Portofolio
+        </h1>
+      </div>
+      <ul className={`flex items-center gap-4 fixed transition-all duration-300 ease-in-out left-1/2 -translate-x-1/2 bg-white/30 backdrop-blur-md p-4 rounded-br-2xl rounded-bl-2xl sm:gap-10 md:-translate-x-0 md:static md:opacity-100 md:top-0 ${active ? 'top-0 opacity-100' : '-top-10 opacity-0'}`}>
+        <li>
+          <a href="" className='sm:text-lg text-base font-medium'>Beranda</a>
+        </li>
+        <li>
+          <a href="" className='sm:text-lg text-base font-medium'>Tentang</a>
+        </li>
+        <li>
+          <a href="" className='sm:text-lg text-base font-medium'>Proyek</a>
+        </li>
+        <li>
+          <a href="" className='sm:text-lg text-base font-medium'>Kontak</a>
+        </li>
+      </ul>
+    </div>
+  )
+}
+
+export default Navbar
